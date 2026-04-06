ae685794d564dab27c0fa806b7b1aaf265a53d72
sewminiwijesiri <sewminiwijesiri5@gmail.com>
2026-04-06 11:45:47 +0530

    refactor: modularize express application into app.js and add rate limiting middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/db");
 
 require("dotenv").config();
 
@@ -20,13 +19,9 @@ app.use(limiter);
 app.use(cors());
 app.use(express.json());
 
-connectDB();
-
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/records", require("./routes/recordRoutes"));
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log("Server running...");
-});
+module.exports = app;
