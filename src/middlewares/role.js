bd61d9fc545eaeb76c7c4b232487dd233592dbcc
sewminiwijesiri <sewminiwijesiri5@gmail.com>
2026-04-06 10:05:03 +0530

    feat: implement JWT authentication and role-based access control middlewares

---
+++ src/middlewares/role.js
@@ -0,0 +1,8 @@
+module.exports = (...allowed) => {
+    return (req, res, next) => {
+        if (!allowed.includes(req.user.role)) {
+            return res.status(403).json({ message: "Access denied" });
+        }
+        next();
+    };
+};
